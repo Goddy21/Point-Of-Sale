b316484853c52a2b382bb6211aaf9d1b62aaa3e8
Goddy21 <116700472+Goddy21@users.noreply.github.com>
2024-10-03 10:01:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from admin.admin import AdminWindow
 from signin.signin import SigninWindow
+from signup.signup import SignupWindow
 from till_operator.till_operator import OperationWindow
 from kivy.config import Config
 
@@ -16,12 +17,14 @@ Config.set('kivy', 'window', 'sdl2')
 class MainWindow(BoxLayout):
     admin_widget = AdminWindow()
     signin_widget = SigninWindow()
+    signup_widget = SignupWindow()
     operator_widget = OperationWindow()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.ids.scrn_si.add_widget(self.signin_widget)
+        self.ids.scrn_signup.add_widget(self.signup_widget)
         self.ids.scrn_admin.add_widget(self.admin_widget)
         self.ids.scrn_op.add_widget(self.operator_widget)
 
